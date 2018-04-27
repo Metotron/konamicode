7fa1f9afbd6eb8c2da049ba168dd311e933e1852
Metotron <metotron1@yahoo.com>
2018-04-27 04:14:31 +0300

    Сам файл

---
+++ konami.js
@@ -0,0 +1,13 @@
+function KonamiKode(callback) {
+	const code = ['ArrowUp', 'ArrowUp', 'ArrowDown', 'ArrowDown', 'ArrowLeft', 'ArrowRight', 'ArrowLeft', 'ArrowRight', 'b', 'a']
+	let stage = 0, done = code.length 
+
+	window.addEventListener('keydown', function(e) {
+		if (stage == done) return
+		if (e.key == code[stage]) ++stage
+		else stage = 0
+
+		if (stage == done && callback !== undefined)
+			callback.call()
+	}, false);
+}
